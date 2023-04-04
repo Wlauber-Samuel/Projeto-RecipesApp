d5a85ed4b4983e0592fd73172e5d90d778436c54
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-04-04 17:14:33 -0300

    Requisitos profile (57 ao 62) finalizados

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+  const email = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Profile" />
+      <p data-testid="profile-email">{email.email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
---
+++ src/tests/Profile.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import createMemoryHistory from 'history/createMemoryHistory';
+import App from '../App';
+
+describe('Testes para o componente Footer', () => {
+  test('Teste se o footer contém os 3 ícones e 3 textos', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const profileBtn = 'profile-top-btn';
+    userEvent.type(screen.getByTestId('email-input'), 'email@teste.com');
+    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.click(screen.getByTestId('login-submit-btn'));
+    userEvent.click(screen.getByTestId(profileBtn));
+    expect(screen.getByText('Profile')).toBeInTheDocument();
+    expect(screen.getByText('email@teste.com')).toBeInTheDocument();
+    expect(screen.getByTestId('profile-email')).toBeInTheDocument();
+    expect(screen.getByTestId('profile-done-btn')).toBeInTheDocument();
+    expect(screen.getByTestId('profile-favorite-btn')).toBeInTheDocument();
+    expect(screen.getByTestId('profile-logout-btn')).toBeInTheDocument();
+    userEvent.click(screen.getByTestId('profile-done-btn'));
+    expect(screen.getByText('Done Recipes')).toBeInTheDocument();
+    userEvent.click(screen.getByTestId(profileBtn));
+    userEvent.click(screen.getByText('Favorite Recipes'));
+    expect(screen.getByText('Favorite Recipes')).toBeInTheDocument();
+    userEvent.click(screen.getByTestId(profileBtn));
+    userEvent.click(screen.getByTestId('profile-logout-btn'));
+    expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
+  });
+});
