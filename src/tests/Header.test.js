1f476cab65b8460b7d8dfb2458b3981615e10d3b
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-04-06 18:09:17 -0300

    deixando tudo em componentes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
-import createMemoryHistory from 'history/createMemoryHistory';
 import App from '../App';
 
 describe('Testes para o componente Footer', () => {
