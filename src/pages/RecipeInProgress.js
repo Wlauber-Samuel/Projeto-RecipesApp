224c897816071735a8757697a57d6657c1717e9d
Heitor Soares <heytorpg7@gmail.com>
2023-04-10 02:24:20 -0300

    feat: list marking

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,12 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
-import Header from '../components/Header';
 import Erro from '../components/Erro';
 import useFetch from '../hooks/useFetch';
 import Loading from '../components/Loading';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+import './RecipeInProgress.css';
 
 const MEAL_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const DRINK_URL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
@@ -16,6 +16,7 @@ function RecipeInProgress() {
   const { id } = useParams();
   const { pathname } = useLocation();
   const [favorite, setFavorite] = useState(false);
+  const [markedList, setMarkedList] = useState({});
   const [parsedData, setParsedData] = useState({
     name: '',
     image: '',
@@ -52,6 +53,16 @@ function RecipeInProgress() {
     console.log('favorite!!');
   };
 
+  const handleMarking = ({ target: { checked, parentNode } }) => {
+    if (checked) {
+      parentNode.className = 'marked';
+      console.log(parentNode.textContent);
+    } else {
+      console.log(parentNode.textContent);
+      parentNode.className = 'unmarked';
+    }
+  };
+
   // Effects
   useEffect(() => {
     const requestData = async () => {
@@ -69,62 +80,66 @@ function RecipeInProgress() {
     }
   }, [isLoading, handleData, errors]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   if (errors) {
     return <Erro message={ errors.message } />;
   }
 
   return (
-    <div>
-      <div className="buttons__container">
-        <button data-testid="share-btn" onClick={ handleShare }>
-          <img src={ shareIcon } alt="compartilhar" />
-        </button>
+    isLoading ? <Loading />
+      : (
+        <div>
+          <div className="buttons__container">
+            <button data-testid="share-btn" onClick={ handleShare }>
+              <img src={ shareIcon } alt="compartilhar" />
+            </button>
 
-        <button onClick={ handleClickFavorite }>
+            <button onClick={ handleClickFavorite }>
+              <img
+                src={ !favorite ? whiteHeartIcon : blackHeartIcon }
+                alt="compartilhar"
+                data-testid="favorite-btn"
+              />
+            </button>
+          </div>
+
+          <h6 data-testid="recipe-category">{ parsedData.type }</h6>
+          <h1 data-testid="recipe-title">{ parsedData.name }</h1>
+
           <img
-            src={ !favorite ? whiteHeartIcon : blackHeartIcon }
-            alt="compartilhar"
-            data-testid="favorite-btn"
+            src={ parsedData.image }
+            alt={ parsedData.name }
+            data-testid="recipe-photo"
+            width="250px"
           />
-        </button>
-      </div>
 
-      <h6 data-testiod="recipe-category">{ parsedData.type }</h6>
-      <h1 data-testid="recipe-title">{ parsedData.name }</h1>
+          <section>
+            <h2>Ingredients</h2>
 
-      <img
-        src={ parsedData.image }
-        alt={ parsedData.name }
-        data-testid="recipe-photo"
-        width="250px"
-      />
+            <ul className="list-reset">
+              {
+                parsedData.ingredients.map((item, index) => (
+                  <li key={ index }>
+                    <label data-testid={ `${index}-ingredient-step` }>
+                      <input
+                        type="checkbox"
+                        onChange={ handleMarking }
+                      />
+                      { item }
+                    </label>
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
 
-      <div>
-        <h2>Ingredients</h2>
+          <section>
+            <h2>Instructions</h2>
+            <p data-testid="instructions">{ parsedData.instructions }</p>
+          </section>
 
-        <ul style={ { listStyle: 'none', padding: 0 } }>
-          {parsedData.ingredients.map((item, index) => (
-            <li key={ index }>
-              <label htmlFor={ `ingredient-${index}` } style={ { textDecoration: 'line-through' } }>
-                <input type="checkbox" id={ `ingredient-${index}` } />
-                { item }
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div>
-        <h2>Instructions</h2>
-        <p data-testid="instructions">{ parsedData.instructions }</p>
-      </div>
-
-      <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
-    </div>
+          <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+        </div>
+      )
   );
 }
 
