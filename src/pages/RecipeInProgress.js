0bb1abdeb308a5eef09ab1ad0dce6b80c3e1d3e4
Heitor Soares <heytorpg7@gmail.com>
2023-04-12 13:11:17 -0300

    merging stash

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -56,6 +56,14 @@ function RecipeInProgress() {
   }, [data]);
 
   const handleShare = () => {
+    console.log('share!');
+  };
+
+  const handleClickFavorite = () => {
+    console.log('favorite!!');
+  };
+
+  const handleShare = () => {
 
   };
 
